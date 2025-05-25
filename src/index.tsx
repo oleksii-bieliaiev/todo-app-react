5f7d8c4376aeb74b808d3cd6be45f85963d63fd5
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2025-05-25 23:09:04 +0200

    app init

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import ReactDOM from 'react-dom';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { TodoApp } from './components/TodoApp';
+
+ReactDOM.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<TodoApp />} />
+        <Route path="completed" element={<TodoApp />} />
+        <Route path="active" element={<TodoApp />} />
+      </Route>
+    </Routes>
+  </HashRouter>,
+  document.getElementById('root'),
+);
